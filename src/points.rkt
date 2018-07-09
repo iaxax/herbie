f87499e958c674c7e4978f80bc3c37878727c054
DavidThien <dthien@cs.washington.edu>
2018-07-09 10:47:47 -0700

    Updated baseline regime calculation to use the intial context

--- src/points.rkt
+++ src/points.rkt
@@ -292,9 +292,10 @@
   (for/list ([(point exact) (in-pcontext pcontext)])
     (argmin identity (map (λ (alt) (point-error (alt point) exact)) unique-alts))))
 
-(define (baseline-error alt-bodies pcontext)
+(define (baseline-error alt-bodies pcontext newpcontext)
   (define unique-alts (remove-duplicates alt-bodies))
-  (argmin identity (map (λ (alt) (errors-score (eval-errors alt pcontext))) alt-bodies)))
+  (define baseline (argmin (λ (alt) (errors-score (eval-errors alt pcontext))) alt-bodies))
+  (errors-score (eval-errors baseline newpcontext)))
 
 (define (errors prog pcontext)
   (eval-errors (eval-prog prog 'fl) pcontext))
